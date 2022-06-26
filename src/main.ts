cef244cf6a18af2c09421c81fefb2694fe768f04
Levy <levyronald3@gmail.com>
2022-06-26 14:44:27 -0300

    feat: Adicionando arquivos de validação

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
   const config = new DocumentBuilder()
   .setTitle('Cats example')
   .setDescription('The cats API description')
--- src/users/models/users.model.ts
+++ src/users/models/users.model.ts
@@ -1,22 +1,31 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, } from 'class-validator';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
-
 @Schema()
 @ApiTags('users')
 export class User {
   @Prop()
   @ApiProperty()
+  @IsNotEmpty({
+    message: 'Digite um nome de Usuário'
+  })
   name: string;
 
   @Prop()
   @ApiProperty()
+  @IsEmail({}, {
+    message: 'Digite um endereço de email.'
+  })
   email: string;
-
+  
   @Prop()
   @ApiProperty()
+  @IsNotEmpty({
+    message: 'Digite uma senha'
+  })
   password: string;
 }
 
